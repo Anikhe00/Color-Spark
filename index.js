39579acfb8c3a8f4c9bb09d7abd4fb80bc2fed6c
Shakirat Akanji <akanjishakirato@gmail.com>
2025-01-29 17:44:16 +0100

    Reset Button Added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hexaDecimalNumbers = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
 let button = document.getElementById("generate-button")
 let textColor = "#FFFFFF"
+let resetButton = document.getElementById("reset-button")
 
 // First Color
 let firstColor = document.getElementById("first-color")
@@ -19,6 +20,13 @@ let thirdHexCode = document.getElementById("third-color-hex-code")
 let fourthColor = document.getElementById("fourth-color")
 let fourthHexCode = document.getElementById("fourth-color-hex-code")
 
+// Original Colors
+let firstBoxOriginalColor = "#F3F4F6"
+let secondBoxOriginalColor = "#E8EAED"
+let thirdBoxOriginalColor = "#DCE0E5"
+let fourthBoxOriginalColor = "#D0D5DC"
+let textOriginalColor = "#202733"
+
 
 
 // Generate random numbers from the hexaDecimalNumbers
@@ -26,6 +34,24 @@ function generateRandomNumber(){
   let randomNumber = Math.floor(Math.random() * hexaDecimalNumbers.length)
   return randomNumber
 }
+
+// Button Hover
+button.addEventListener("mouseover", function(){
+  button.style.backgroundColor = "#475671"
+})
+
+button.addEventListener("mouseleave", function(){
+  button.style.backgroundColor = "#333E51"
+})
+
+// Reset Button Hover
+resetButton.addEventListener("mouseover", function(){
+  resetButton.style.backgroundColor = "#B4BED0"
+})
+
+resetButton.addEventListener("mouseleave", function(){
+  resetButton.style.backgroundColor = "#D3D9E3"
+})
 
 button.addEventListener("click", function(){
   // First Color Box
@@ -63,4 +89,28 @@ button.addEventListener("click", function(){
   fourthColor.style.backgroundColor = fourthColorHexCode
   fourthHexCode.textContent = fourthColorHexCode
   fourthHexCode.style.color = textColor
+})
+
+// Reset
+resetButton.addEventListener("click", function(){
+
+  // First box
+  firstColor.style.backgroundColor = firstBoxOriginalColor
+  firstHexCode.textContent = firstBoxOriginalColor
+  firstHexCode.style.color = textOriginalColor
+
+  // Second box
+  secondColor.style.backgroundColor = secondBoxOriginalColor
+  secondHexCode.textContent = secondBoxOriginalColor
+  secondHexCode.style.color = textOriginalColor
+
+  // Third box
+  thirdColor.style.backgroundColor = thirdBoxOriginalColor
+  thirdHexCode.textContent = thirdBoxOriginalColor
+  thirdHexCode.style.color = textOriginalColor
+
+  // Fourth box
+  fourthColor.style.backgroundColor = fourthBoxOriginalColor
+  fourthHexCode.textContent = fourthBoxOriginalColor
+  fourthHexCode.style.color = textOriginalColor
 })
